2c56e04c3343b69678ead1cad029abd3ddca74b1
Phuochoit <phuocck8@gmail.com>
2017-12-01 22:39:06 +0700

    upload verstion 1.1.3

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import {
-    Container,
-    Header,
-    Body,
-    Button,
-    Icon,
-    Title,
-    Input,
-    Item,
-    Text,
-    Toast
-} from 'native-base';
+import { Container, Header, Body, Button, Icon, Title, Input, Text, Toast } from 'native-base';
 import { Clipboard, View, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native';
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { isEmpty } from "lodash";
 
 import { tieqViet } from "../api/service";
 import { AdMobBannerHeader, AdMobBannerFooter } from "./admob";
-import { DismissKeyboard } from "./dismissKeyboar";
-
 
 class Home_Component extends Component {
     constructor(props) {
@@ -51,9 +38,8 @@ class Home_Component extends Component {
 
     render() {
         const newstring = tieqViet(this.state.dataString);
-        string = <View style={{ flex: 40 }} />;
+        string = <View style={{ flex: 1 }} />;
         button = <View style={{ flex: 1 }} />;
-        const DismissKeyboardView = DismissKeyboard(View)
 
         if (!isEmpty(newstring)) {
             button = (
@@ -83,72 +69,62 @@ class Home_Component extends Component {
             );
         }
         return (
-            <View style={{ flex: 1 }}>
+            <Container>
                 <Header style={{ backgroundColor: "#f55020" }}>
                     <Body style={{ alignItems: 'center' }}>
                         <Title>Bộ chuyển đổi Tiếq Việt</Title>
                     </Body>
                 </Header>
-                <View style={{
-                    flex: 1,
-                    backgroundColor: '#FFFFFF',
-                    paddingHorizontal: 10,
-
-                }}>
+                <View style={{ flex: 1, paddingHorizontal: 10, backgroundColor: "#fff" }}>
                     <View style={{ flex: 50 }}>
-                        <AdMobBannerHeader bannerSize="banner" />
-                        <Input
-                            placeholder='Gõ tiếng Việt vào đây nào các bạn'
-                            value={this.state.dataString}
-                            onChangeText={
-                                text => this.setState({
-                                    dataString: text
-                                })
-                            }
-                            textAlignVertical="top"
-                            multiline={true}
-                            style={{
-                                height: 100,
-                                backgroundColor: '#F5F5F5',
-                                padding: 10,
-                                borderRadius: 6,
-                                flex:80
-                            }}
-                        />
-                        <Button iconLeft
-                            onPress={this._OnClickClear}
-                            style={{
-                                paddingHorizontal: 20,
-                                borderRadius: 6,
-                                backgroundColor: '#F55020',
-                                marginVertical: 10,
-                                alignSelf: "flex-end",
-                                flex:10
-                            }}
-                        >
-                            <MaterialIcons name='clear' color="#fff" />
-                            <Text>XÓA TẤT CẢ</Text>
-                        </Button>
-                    </View>
-
-                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                        <View style={{ flex: 50, marginTop: 15 }}>
-                            <View style={{
-                                flex: 70
-                            }}>
-                                {string}
-                            </View>
-                            <AdMobBannerFooter bannerSize="banner" />
-                            <View style={{
-                                flex:10
-                            }}>
-                                {button}
-                            </View>
+                        <View style={{ flex: 80 }}>
+                            <AdMobBannerHeader bannerSize="banner" />
+                            <Input
+                                placeholder='Gõ tiếng Việt vào đây nào các bạn'
+                                value={this.state.dataString}
+                                onChangeText={
+                                    text => this.setState({
+                                        dataString: text
+                                    })
+                                }
+                                textAlignVertical="top"
+                                multiline={true}
+                                style={{
+                                    backgroundColor: '#F5F5F5',
+                                    padding: 10,
+                                    borderRadius: 6,
+                                    flex: 1
+                                }}
+                            />
                         </View>
-                    </TouchableWithoutFeedback>
+                        <View style={{ flex: 20, justifyContent: "center", }}>
+                            <Button iconLeft
+                                onPress={this._OnClickClear}
+                                style={{
+                                    borderRadius: 6,
+                                    backgroundColor: '#F55020',
+                                    alignSelf: "flex-end",
+                                    paddingHorizontal: 20
+                                }}
+                            >
+                                <MaterialIcons name='clear' color="#fff" />
+                                <Text>XÓA TẤT CẢ</Text>
+                            </Button>
+                        </View>
+                    </View>
+                    <View style={{ flex: 50 }}>
+                        <View style={{ flex: 65 }}>
+                            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                                {string}
+                            </TouchableWithoutFeedback>
+                        </View>
+                        <View style={{ flex: 35, justifyContent: "flex-end" }}>
+                            <AdMobBannerFooter bannerSize="banner" />
+                            {button}
+                        </View>
+                    </View>
                 </View>
-
-            </View>
+            </Container>
         );
     }
 }
